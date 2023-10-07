2d603fda4faa8b0c6fd578e28650acb85a2378b2
Paul Falstad <pfalstad@zevx.com>
2023-10-06 20:42:55 -0700

    new motor model

--- src/com/lushprojects/circuitjs1/client/ThreePhaseMotorElm.java
+++ src/com/lushprojects/circuitjs1/client/ThreePhaseMotorElm.java
@@ -17,13 +17,12 @@ class ThreePhaseMotorElm extends CircuitElm {
 
     double coilCurrent;
     double inertiaCurrent;
-    int[] voltSources = new int[2];
     Point posts[], leads[];
     
     public ThreePhaseMotorElm(int xx, int yy) { 
 	super(xx, yy); 
 	inductance = .5; resistance = 1; angle = pi/2; speed = 0; K = 0.15; b= 0.05; J = 0.02; Kb = 0.15; gearRatio=1; tau=0;
-
+        voltSources = new int[2];
     }
     public ThreePhaseMotorElm(int xa, int ya, int xb, int yb, int f, StringTokenizer st) {
 	super(xa, ya, xb, yb, f);
@@ -38,6 +37,7 @@ class ThreePhaseMotorElm extends CircuitElm {
 	b = 		new Double(st.nextToken()).doubleValue();
 	gearRatio = new Double(st.nextToken()).doubleValue();
 	tau = 		new Double(st.nextToken()).doubleValue();
+        voltSources = new int[2];
     }
     int getDumpType() { return 427; }
     String dump() {
@@ -61,100 +61,166 @@ class ThreePhaseMotorElm extends CircuitElm {
     }
     int getPostCount() { return 3; }
     Point getPost(int n) { return posts[n]; }
-    int getInternalNodeCount() { return 4; }
+    int getInternalNodeCount() { return 8; }
+    int getVoltageSourceCount() { return 2; }
     void reset() {
 	super.reset();
+	speed = 0;
+        coilCurSourceValues = new double[coilCount];
+        coilCurrents = new double[coilCount];
     }
 
-    final int phids_ind = 3;
-    final int phidr_ind = 4;
-    final int phiqs_ind = 5;
-    final int phiqr_ind = 6;
+    final int n001_ind = 3;
+    final int n002_ind = 4;
+    final int n003_ind = 5;
+    final int n004_ind = 6;
+    final int n005_ind = 7;
+    final int n006_ind = 8;
+    final int n007_ind = 9;
+    final int nn_ind = 10;
     
-    final double Rs = .435;
-    final double Rr = .816;
+    final double Rs = .067;
+    final double Rr = .032;
+    final double Cj = .64;
+    final double Ls = .0294;
+    final double Lr = .0297;
+    final double Lm = .0287;
+    final double Zp = 2;
+    
+    final int coilCount = 5;
+    
+    // based on https://forum.kicad.info/t/ac-motors-simulation-1-phase-3-phase/14188/3
     
     void stamp() {
 	int i;
-	int phids = nodes[phids_ind];
-	int phidr = nodes[phidr_ind];
-	int phiqs = nodes[phiqs_ind];
-	int phiqr = nodes[phiqr_ind];
 	
-	// https://core.ac.uk/download/pdf/80147763.pdf
-	// dphids/dt = vds - A phids + B phidr
-	// expand dphids/dt as (phids(t+1)-phids(t))/dt
-	// expand vds as sqrt(2/3) (va - .5 vb - .5 vc)
-	double m23 = Math.sqrt(2/3.);
-	double m32 = Math.sqrt(3)/2;
-	double ts = 1/sim.timeStep;
-	double Lr = 1;
-	double Ls = 1;
-	double Lm = .99;
-	double wr = speed*3;
+	int n001 = nodes[n001_ind];
+	int n002 = nodes[n002_ind];
+	int n003 = nodes[n003_ind];
+	int n004 = nodes[n004_ind];
+	int n005 = nodes[n005_ind];
+	int n006 = nodes[n006_ind];
+	int n007 = nodes[n007_ind];
+	int nn   = nodes[nn_ind];
+	sim.console("nodes " + n001 + " " + n002 + " " + n003);
 
-	double den = 1/(Ls*Lr-Lm*Lm);
+	sim.stampResistor(nodes[0], n001, Rs);
+	sim.stampResistor(nodes[1], n003, Rs);
+	sim.stampResistor(nodes[2], n005, Rs);
+	sim.stampResistor(n004, 0, 1.5*Rr);
+	sim.stampResistor(n007, 0, 1.5*Rr);
 	
-	// stationary reference frame so we = 0
-	sim.stampMatrix(phids, nodes[0], m23*ts);
-	sim.stampMatrix(phids, nodes[1], -.5 * m23*ts);
-	sim.stampMatrix(phids, nodes[2], -.5 * m23*ts);
-	sim.stampMatrix(phids, phids, - ts * Rs*Lr*den);
-	sim.stampMatrix(phids, phidr, ts*Rs*Lm*den);
-	
-//	sim.stampMatrix(phiqs, nodes[0], 0);
-	sim.stampMatrix(phiqs, nodes[1], m23* m32 * ts);
-	sim.stampMatrix(phiqs, nodes[2], m23* m32 * ts);
-	sim.stampMatrix(phiqs, phiqs, - ts * Rs*Lr*den);
-	sim.stampMatrix(phiqs, phiqr, ts*Rs*Lm*den);
+	double coilInductances[] = { Ls, Ls, Ls, 1.5*Lr, 1.5*Lr };
+	double couplingCoefs[][] = new double[coilCount][coilCount];
+        xformMatrix = new double[coilCount][coilCount];
 
-	sim.stampMatrix(phidr, phidr, - ts* Rr*Ls*den);
-	sim.stampMatrix(phidr, phids, ts * Rr*Lm*den);
+        // see CustomTransformerElm.java
+        
+        // fill diagonal
+        for (i = 0; i != coilCount; i++)
+            xformMatrix[i][i] = coilInductances[i];
+        
+        couplingCoefs[0][3] = couplingCoefs[3][0] = Lm/Math.sqrt(Ls*1.5*Lr);
+        couplingCoefs[1][3] = couplingCoefs[3][1] = -Lm/(2*Math.sqrt(Ls*1.5*Lr));
+        couplingCoefs[1][4] = couplingCoefs[4][1] = Math.sqrt(3)*Lm/(2*Math.sqrt(Ls*1.5*Lr));
+        couplingCoefs[2][3] = couplingCoefs[3][2] = -Lm/(2*Math.sqrt(Ls*1.5*Lr));
+        couplingCoefs[2][4] = couplingCoefs[4][2] = -Math.sqrt(3)*Lm/(2*Math.sqrt(Ls*1.5*Lr));
+                
+        int j;
+        // fill off-diagonal
+        for (i = 0; i != coilCount; i++)
+            for (j = 0; j != i; j++) {
+                xformMatrix[i][j] = xformMatrix[j][i] = couplingCoefs[i][j]*Math.sqrt(coilInductances[i]*coilInductances[j]);
+                sim.console("xform " + i + " " + j + " "+ xformMatrix[i][j]);
+            }
 	
-	sim.stampMatrix(phiqr, phiqr, - ts * Rr*Ls*den);
-	sim.stampMatrix(phiqr, phiqs, ts*Rr*Lm*den);
+        CirSim.invertMatrix(xformMatrix, coilCount);
+        for (i = 0; i != coilCount; i++)
+            for (j = 0; j != coilCount; j++)
+        	sim.console("xform " + i + " " + j + " "+ xformMatrix[i][j]);
 
-	// ia, ib, ic
-	sim.stampMatrix(nodes[0], phids,  m23 * Lr*den);
-	sim.stampMatrix(nodes[0], phidr, -m23 * Lm*den);
-	sim.stampMatrix(nodes[1], phids, -.5*  m23 * Lr*den);
-	sim.stampMatrix(nodes[1], phidr, -.5* -m23 * Lm*den);
-	sim.stampMatrix(nodes[2], phids, -.5*  m23 * Lr*den);
-	sim.stampMatrix(nodes[2], phidr, -.5* -m23 * Lm*den);
-	
-	sim.stampMatrix(nodes[1], phiqs, m32*  m23 * Lr*den);
-	sim.stampMatrix(nodes[1], phiqr, m32* -m23 * Lm*den);
-	sim.stampMatrix(nodes[2], phiqs, -m32*  m23 * Lr*den);
-	sim.stampMatrix(nodes[2], phiqr, -m32* -m23 * Lm*den);
+        double ts = sim.timeStep;
+        for (i = 0; i != coilCount; i++)
+            for (j = 0; j != coilCount; j++) {
+                // multiply in dt/2 (or dt for backward euler)
+                xformMatrix[i][j] *= ts;
+                int ni1 = coilNodes[i*2];
+                int nj1 = coilNodes[j*2];
+                int ni2 = coilNodes[i*2+1];
+                int nj2 = coilNodes[j*2+1];
+                if (i == j)
+                    sim.stampConductance(nodes[ni1], nodes[ni2], xformMatrix[i][i]);
+                else
+                    sim.stampVCCurrentSource(nodes[ni1], nodes[ni2], nodes[nj1], nodes[nj2], xformMatrix[i][j]);
+            }
+        for (i = 0; i != 10; i++)
+            sim.stampRightSide(nodes[coilNodes[i]]);
+        
+        sim.stampVoltageSource(n002, 0, voltSources[0]);
+        sim.stampVoltageSource(n006, 0, voltSources[1]);
+        
+        coilCurSourceValues = new double[coilCount];
+        coilCurrents = new double[coilCount];
+        int nodeCount = getPostCount() + getInternalNodeCount();
+        nodeCurrents = new double[nodeCount];
     }
     
+    int coilNodes[] = { n001_ind, nn_ind, n003_ind, nn_ind, n005_ind, nn_ind, n002_ind, n004_ind, n006_ind, n007_ind };
+    double coilCurrents[];
+    double coilCurSourceValues[];
+    double xformMatrix[][];
+    double nodeCurrents[];
+    int voltSources[];
+    
+    void setVoltageSource(int n, int v) { voltSources[n] = v; }
+    
     void startIteration() {
-	sim.console("phids = " + volts[3] + ", phidr = " + volts[4]);
-	
-	double wr = speed*3; // 3=number of poles
-	
-	double ts = 1/sim.timeStep;
-	double Lr = 1;
-	double Ls = 1;
-	double Lm = .99;
+        int i;
+        for (i = 0; i != coilCount; i++) {
+            double val = coilCurrents[i];
+            coilCurSourceValues[i] = val;
+        }
+        
+        speed += sim.timeStep * (Zp * Math.sqrt(3)/2 * Lm * (coilCurrents[1]-coilCurrents[2]) * coilCurrents[3] - Math.sqrt(3) * coilCurrents[0] * coilCurrents[4]);
+        angle= angle + speed*sim.timeStep;
 
-	double den = 1/(Ls*Lr-Lm*Lm);
-
-	double phids = volts[phids_ind];
-	double phiqs = volts[phiqs_ind];
-	double ids = Lr * den * volts[phids_ind] - Lm * den * volts[phidr_ind];
-	double iqs = Lr * den * volts[phiqs_ind] - Lm * den * volts[phiqr_ind];
-	double idr = Ls * den * volts[phidr_ind] - Lm * den * volts[phids_ind];
-	double iqr = Ls * den * volts[phiqr_ind] - Lm * den * volts[phiqs_ind];
-	
-	int poles = 3;
-	double torque = 3*poles/4. * Lm * (iqs*idr-ids*iqr);
-	double J = .089;
-	speed += poles*torque*sim.timeStep/(2*J);
-	sim.console("torque = " + torque + " speed = " + speed + " " + sim.t);
-	if (Math.abs(torque) > 1e10)
-	    sim.stop("bad torque", this);
-//	sim.console("phids = " + phids + ", phiqs = " + phiqs + ", ids = " + ids + ", iqs = " + iqs + ", idr = " + idr + ", iqr = " + iqr);
+	int n002 = nodes[n002_ind];
+	int n006 = nodes[n006_ind];
+        sim.updateVoltageSource(n002, 0, voltSources[0], -Zp*speed*(Lm*Math.sqrt(3)/2 * (coilCurrents[1]-coilCurrents[2]) + 1.5*Lr*coilCurrents[4]));
+        sim.updateVoltageSource(n006, 0, voltSources[1], Zp*speed*(3/2.*Lm*coilCurrents[0] + 1.5*Lr*coilCurrents[3]));
+        sim.console("iter " + speed);
+    }
+    
+    void doStep() {
+        int i;
+        for (i = 0; i != coilCount; i++) {
+            int n1 = coilNodes[i*2];
+            int n2 = coilNodes[i*2+1];
+            sim.stampCurrentSource(nodes[n1], nodes[n2], coilCurSourceValues[i]);
+        }
+    }
+    
+    void calculateCurrent() {
+        int i;
+        int nodeCount = getPostCount() + getInternalNodeCount();
+        for (i = 0; i != nodeCount; i++)
+            nodeCurrents[i] = 0;
+        for (i = 0; i != coilCount; i++) {
+            double val = coilCurSourceValues[i];
+            if (xformMatrix != null) {
+                int j;
+                for (j = 0; j != coilCount; j++) {
+                    int n1 = coilNodes[j*2];
+                    int n2 = coilNodes[j*2+1];
+                    double voltdiff = volts[n1]-volts[n2];
+                    val += voltdiff*xformMatrix[i][j];
+                }
+            }
+            coilCurrents[i] = val;
+            int ni = coilNodes[i];
+            nodeCurrents[ni] += val;
+            nodeCurrents[ni+1] -= val;
+        }
     }
 
     boolean hasGroundConnection(int n1) {
@@ -163,37 +229,6 @@ class ThreePhaseMotorElm extends CircuitElm {
     
     boolean getConnection(int n1, int n2) {
     	return true;
-    }
-    
-    void doStep() {
-	int phids = nodes[phids_ind];
-	int phidr = nodes[phidr_ind];
-	int phiqs = nodes[phiqs_ind];
-	int phiqr = nodes[phiqr_ind];
-	sim.stampRightSide(phidr, volts[phidr_ind]);
-	sim.stampRightSide(phids, volts[phids_ind]);
-	sim.stampRightSide(phiqs, volts[phiqs_ind]);
-	sim.stampRightSide(phiqr, volts[phiqr_ind]);
-	
-	double ts = 1/sim.timeStep;
-	double wr = speed*3;
-
-	// time-varying part of matrix
-	sim.stampMatrix(phidr_ind, phiqr_ind, -wr*ts);
-	sim.stampMatrix(phiqr_ind, phidr_ind, -wr*ts);
-    }
-    
-    void calculateCurrent() {
-//	inds[0].calculateCurrent(volts[5]-volts[6]);
-//	inds[1].calculateCurrent(volts[6]-volts[7]);
-//	inds[2].calculateCurrent(volts[10]-volts[11]);
-//	inds[3].calculateCurrent(volts[11]-volts[12]);
-    }
-//    public double getCurrent() { current = (volts[2]-volts[3])/resistance; return current; }
-
-    void setCurrent(int vn, double c) {
-	if (vn == voltSources[0])
-	    current = c;
     }
     
     void draw(Graphics g) {
